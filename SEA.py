20bc13da9588d77030b6605dea67c4dd9858b43b
neuromancer <daneel@localhost.localdomain>
2013-04-13 20:02:54 -0300

    fixed license

--- SEA.py
+++ SEA.py
@@ -3,12 +3,12 @@
 """
     This file is part of SEA.
 
-    reserbot is free software: you can redistribute it and/or modify
+    SEA is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    reserbot is distributed in the hope that it will be useful,
+    SEA is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
