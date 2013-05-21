688ab1645c6cc0247ba4bdef1afb1dad4d3f8159
neuromancer <sistemas@SI-10.(none)>
2013-05-21 16:35:32 -0300

    updated two-loops example

--- tests/c/two-loops.c
+++ tests/c/two-loops.c
@@ -1,14 +1,20 @@
-int main(int argc) {
+void f(int x)  {
 
   int i;
-  for (i=0; i<3; i++)
+  for (i=0; i<x; i++)
     printf("%d",i);
   
-  if (argc > 1)
-    printf("\nargc > 1\n");
+  if (x > 1)
+    printf("\nx > 1\n");
 
-  for (i=0; i<5; i++)
+  for (i=0; i<x*2; i++)
     printf("%d",i);
+
+}
+
+
+int main(int argc) {
   
+  f(argc);
   return 0;
 }
